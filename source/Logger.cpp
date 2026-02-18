252b839aa5595416f6ecf00ce678e0932a910425
warpersan <leumas.ecole@gmail.com>
2026-02-18 14:54:31 -0500

    Added Logger outline

---
+++ source/Logger.cpp
@@ -0,0 +1,18 @@
+#include "../include/Logger.h"
+
+#include <stdexcept>
+
+Logger::Logger(std::ostream& output)
+{
+	throw std::logic_error("Function not yet implemented");
+}
+
+void Logger::log(std::string_view message)
+{
+	throw std::logic_error("Function not yet implemented");
+}
+
+void Logger::logException(const std::exception& exception)
+{
+	throw std::logic_error("Function not yet implemented");
+}
