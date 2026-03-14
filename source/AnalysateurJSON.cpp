7d3dc8dc0a5d2845b70ef47d9ee0b53b6917f3a3
mano <rod15ilboudo@gmail.com>
2026-03-14 15:40:36 -0500

    jsn.hpp et analysateurJson
    ajout jsn.hpp and implementation methode analysateurJson

--- source/AnalysateurJSON.cpp
+++ source/AnalysateurJSON.cpp
@@ -1,4 +1,6 @@
 #include "../include/AnalysateurJSON.h"
+#include <include/json.hpp>
+using json = nlohmann::json;
 
 #include <stdexcept>
 
@@ -14,5 +16,37 @@ AnalysateurJSON::~AnalysateurJSON()
 
 DonneesArticle AnalysateurJSON::analyser(std::string texte)
 {
+	DonneesArticle article;
+
+    json j = json::parse(texte);
+
+    if (!j.contains("nom") || !j.contains("prix")) {
+        throw std::runtime_error("JSON invalide : champs obligatoires absents.");
+    }
+
+    article.mettreNom(j.at("nom").get<std::string>());
+    article.mettrePrix(j.at("prix").get<double>());
+
+    if (j.contains("proprietes") && j["proprietes"].is_object()) {
+        for (auto& [cle, valeur] : j["proprietes"].items()) {
+            if (valeur.is_string()) {
+                article.mettrePropriete(cle, valeur.get<std::string>());
+            }
+            else if (valeur.is_number_integer()) {
+                article.mettrePropriete(cle, valeur.get<int>());
+            }
+            else if (valeur.is_number_float()) {
+                article.mettrePropriete(cle, valeur.get<double>());
+            }
+            else if (valeur.is_boolean()) {
+                article.mettrePropriete(cle, valeur.get<bool>());
+            }
+            else {
+                article.mettrePropriete(cle, valeur.dump());
+            }
+        }
+    }
+
+    return article;
 	throw std::logic_error("Function not yet implemented");
 }
