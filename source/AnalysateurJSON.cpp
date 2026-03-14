0f3a446c9ac7074a6546f2ee89bffd49c4b98ae3
mano <rod15ilboudo@gmail.com>
2026-03-14 16:21:51 -0500

    mise jour json

--- source/AnalysateurJSON.cpp
+++ source/AnalysateurJSON.cpp
@@ -20,30 +20,29 @@ DonneesArticle AnalysateurJSON::analyser(std::string texte)
 
     json j = json::parse(texte);
 
-    if (!j.contains("nom") || !j.contains("prix")) {
-        throw std::runtime_error("JSON invalide : champs obligatoires absents.");
-    }
-
+    
     article.mettreNom(j.at("nom").get<std::string>());
     article.mettrePrix(j.at("prix").get<double>());
 
-    if (j.contains("proprietes") && j["proprietes"].is_object()) {
-        for (auto& [cle, valeur] : j["proprietes"].items()) {
-            if (valeur.is_string()) {
-                article.mettrePropriete(cle, valeur.get<std::string>());
-            }
-            else if (valeur.is_number_integer()) {
-                article.mettrePropriete(cle, valeur.get<int>());
-            }
-            else if (valeur.is_number_float()) {
-                article.mettrePropriete(cle, valeur.get<double>());
-            }
-            else if (valeur.is_boolean()) {
-                article.mettrePropriete(cle, valeur.get<bool>());
-            }
-            else {
-                article.mettrePropriete(cle, valeur.dump());
-            }
+    for (auto& [cle, valeur] : j.items()) {
+        if (cle == "nom" || cle == "prix") {
+            continue;
+        }
+
+        if (valeur.is_string()) {
+            article.mettrePropriete(cle, valeur.get<std::string>());
+        }
+        else if (valeur.is_number_integer()) {
+            article.mettrePropriete(cle, valeur.get<int>());
+        }
+        else if (valeur.is_number_float()) {
+            article.mettrePropriete(cle, valeur.get<double>());
+        }
+        else if (valeur.is_boolean()) {
+            article.mettrePropriete(cle, valeur.get<bool>());
+        }
+        else {
+            article.mettrePropriete(cle, valeur.dump());
         }
     }
 
