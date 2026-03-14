31205fc100ecf0fb3f7b0acaf8d7d82627e878f2
mano <rod15ilboudo@gmail.com>
2026-03-14 16:35:59 -0500

    implementation de donneeArticle.

--- source/DonneesArticle.cpp
+++ source/DonneesArticle.cpp
@@ -2,7 +2,7 @@
 
 #include <stdexcept>
 
-DonneesArticle::DonneesArticle()
+DonneesArticle::DonneesArticle(): nom(""), prix(0.0)
 {
 	throw std::logic_error("Function not yet implemented");
 }
@@ -14,35 +14,49 @@ DonneesArticle::~DonneesArticle()
 
 void DonneesArticle::mettreNom(std::string nom)
 {
+	 this->nom = nom;
 	throw std::logic_error("Function not yet implemented");
 }
 
 std::string DonneesArticle::obtenirNom()
 {
+	return nom;
 	throw std::logic_error("Function not yet implemented");
 }
 
 void DonneesArticle::mettrePrix(double prix)
 {
+	this->prix = prix;
 	throw std::logic_error("Function not yet implemented");
 }
 
 double DonneesArticle::obtenirPrix()
 {
 	throw std::logic_error("Function not yet implemented");
+	return prix;
 }
 
 void DonneesArticle::mettrePropriete(std::string nom, std::any valeur)
 {
+	proprietes[nom] = valeur;
 	throw std::logic_error("Function not yet implemented");
+	
 }
 
 std::any DonneesArticle::obtenirPropriete(std::string nom)
 {
+	 auto resultat = proprietes.find(nom);
+	 return resultat->second;
 	throw std::logic_error("Function not yet implemented");
 }
 
 std::vector<std::string> DonneesArticle::obtenirNomProprietes()
-{
+{   std::vector<std::string> noms;
+
+	for (const auto& paire : proprietes) {
+        noms.push_back(paire.first);
+    }
+
+    return noms;
 	throw std::logic_error("Function not yet implemented");
 }
