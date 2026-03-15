d817142c7f6356aa531f6702e6f6b1e35eb1cfc6
WarperSan <leumas.ecole@gmail.com>
2026-03-14 22:17:54 -0400

    Implemented `Client`

--- source/Client.cpp
+++ source/Client.cpp
@@ -1,18 +1,24 @@
 #include "../include/Client.h"
 
-#include <stdexcept>
+#include "../include/Enregistreur.h"
 
-Client::Client()
-{
-	throw std::logic_error("Function not yet implemented");
-}
+Client::Client() = default;
 
-Client::~Client()
-{
-	throw std::logic_error("Function not yet implemented");
-}
+Client::~Client() = default;
 
 void Client::transfererArticle(int id)
 {
-	throw std::logic_error("Function not yet implemented");
+	try
+	{
+		const auto rapport = exportateur.genererRapport(id);
+		const auto article = analysateurJSON.analyser(rapport);
+
+		validateurArticle.validerArticle(article);
+
+		importateur.importer(article);
+	}
+	catch (const std::exception& e)
+	{
+		Enregistreur::enregistrer(e);
+	}
 }
