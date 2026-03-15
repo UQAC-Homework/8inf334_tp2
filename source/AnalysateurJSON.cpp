6764f34a7775259fec6dabbaf2614b5d7300e651
WarperSan <leumas.ecole@gmail.com>
2026-03-14 22:17:23 -0400

    Replaced exceptions in constructors and destructors with logs

--- source/AnalysateurJSON.cpp
+++ source/AnalysateurJSON.cpp
@@ -2,14 +2,16 @@
 
 #include <stdexcept>
 
+#include "../include/Enregistreur.h"
+
 AnalysateurJSON::AnalysateurJSON()
 {
-	throw std::logic_error("Function not yet implemented");
+	Enregistreur::enregistrer("[ERROR] Function not yet implemented");
 }
 
 AnalysateurJSON::~AnalysateurJSON()
 {
-	throw std::logic_error("Function not yet implemented");
+	Enregistreur::enregistrer("[ERROR] Function not yet implemented");
 }
 
 DonneesArticle AnalysateurJSON::analyser(std::string texte)
