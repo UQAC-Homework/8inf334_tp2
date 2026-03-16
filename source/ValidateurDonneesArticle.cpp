7fe4ac0b1b68b3f9f23ce5521206f2c1508a0b88
hassy <hassydll@gmail.com>
2026-03-15 23:05:57 -0400

    message changement

--- source/ValidateurDonneesArticle.cpp
+++ source/ValidateurDonneesArticle.cpp
@@ -3,18 +3,38 @@
 #include <stdexcept>
 
 #include "../include/Enregistreur.h"
+#include "../include/ DonneesArticle.h"
 
 ValidateurDonneesArticle::ValidateurDonneesArticle()
 {
-	Enregistreur::enregistrer("[ERROR] Function not yet implemented");
+	Enregistreur::enregistrer("ValidateurDonneesArticle intialiser.");
 }
 
 ValidateurDonneesArticle::~ValidateurDonneesArticle()
 {
-	Enregistreur::enregistrer("[ERROR] Function not yet implemented");
+	Enregistreur::enregistrer("ValidateurDonneesArticle destruite");
 }
 
-void ValidateurDonneesArticle::validerArticle(const DonneesArticle& article)
+void ValidateurDonneesArticle::validerArticle(const DonneesArticle& article) const
 {
-	throw std::logic_error("Function not yet implemented");
+	std::string nom =article.obtenirNom();
+	double prix = article.obtenirPrix();
+	// debut de la validation
+	Enregistreur::enregistrer(nom);
+	// verifiaction le nom doit etre remplie
+	// verification si le nom est vide
+
+	if (nom.empty()) {
+		Enregistreur::enregistrer("erreur: le nom est vide");
+		throw std::invalid_argument("erreur: nom est vide");
+	}
+      // le prix doit etre positif
+	 else if (prix<=0.0) {
+		 Enregistreur::enregistrer("prix invalide");
+	 	throw std::invalid_argument("prix invalide");
+	 }
+	else {
+		Enregistreur::enregistrer("Article valide:"+nom);
+	}
+
 }
