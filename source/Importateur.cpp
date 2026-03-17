61b0847813d39d1316bdb18d3bae932d9ede0d61
WarperSan <leumas.ecole@gmail.com>
2026-03-17 18:32:28 -0400

    Code

--- source/Importateur.cpp
+++ source/Importateur.cpp
@@ -1,20 +1,41 @@
 #include "../include/Importateur.h"
-
-#include <stdexcept>
-
 #include "../include/Enregistreur.h"
+
+#include <string>
+#include <vector>
+#include <exception>
 
 Importateur::Importateur()
 {
-	Enregistreur::enregistrer("[ERROR] Function not yet implemented");
 }
 
 Importateur::~Importateur()
 {
-	Enregistreur::enregistrer("[ERROR] Function not yet implemented");
 }
 
 void Importateur::importer(const DonneesArticle& article)
 {
-	throw std::logic_error("Function not yet implemented");
+	try
+	{
+		Enregistreur::enregistrer("Debut importation article");
+
+		std::string nom = article.obtenirNom();
+		double prix = article.obtenirPrix();
+
+		Enregistreur::enregistrer("Nom article : " + nom);
+		Enregistreur::enregistrer("Prix article : " + std::to_string(prix));
+
+		std::vector<std::string> proprietes = article.obtenirNomProprietes();
+
+		for (const std::string& prop : proprietes)
+		{
+			Enregistreur::enregistrer("Propriete : " + prop);
+		}
+
+		Enregistreur::enregistrer("Importation terminee");
+	}
+	catch (const std::exception& e)
+	{
+		Enregistreur::enregistrer(e);
+	}
 }
